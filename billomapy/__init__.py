f2130802fa7c3d1ce6470db8ae3e299fc5dc81a2
Michael Bykovski <bykof@me.com>
2015-06-23 14:39:54 +0200

    Modified for Pypi

---
+++ billomapy/__init__.py
@@ -0,0 +1,1 @@
+from .billomapy import Billomapy
--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='billomapy',
-    version='0.1',
-    install_requires=open('requirements.txt').read().splitlines(),
-    packages=['api'],
+    version='0.4',
+    install_requires=['requests==2.7.0'],
+    packages=['billomapy'],
     url='https://github.com/bykof/billomapy',
     license='Apache License 2.0',
-    author='mbykovski',
+    author='Michael Bykovski',
     author_email='mbykovski@seibert-media.net',
     description='A Python library for http://www.billomat.com/'
 )
