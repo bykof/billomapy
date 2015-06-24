d6b0d4ebbab3eb14ce79436dccba7ae8f4e0ea56
Michael Bykovski <bykof@me.com>
2015-06-24 18:47:21 +0200

    Updated and Uploaded to PyPi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='billomapy',
-    version='0.5',
+    version='0.6',
     install_requires=['requests==2.7.0'],
     packages=['billomapy'],
     url='https://github.com/bykof/billomapy',
