2e7136fd180d48a9b5862b8bdf64ca7ffc6e972f
Michael Bykovski <bykof@me.com>
2017-10-23 18:44:41 +0200

    VERSION PUSH

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='billomapy',
-    version='2.4',
+    version='2.5',
     install_requires=['requests==2.7.0', 'tornado==4.2'],
     packages=['billomapy'],
     url='https://github.com/bykof/billomapy',
